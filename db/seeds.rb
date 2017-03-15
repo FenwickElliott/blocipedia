dc615bc48259cfa74731c317df4d1ee4911672d2
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-03-15 14:43:37 -0400

    seeding fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'random_data'
+
+user = User.new
+user.update_attributes(
+    email: 'charles@FenwickElliott.io',
+    password: '123456'
+    )
 
 for i in 1..25 do
   Wiki.create!(
-    title: i.to_s << ': Seeded Wiki',
-    body: 'Body of seeds',
-    # user: User.first,
+    title: 'SW_' << i.to_s << ': ' << RandomData.random_sentence,
+    body: 'Body: ' << RandomData.random_paragraph,
     user_id: 1
   )
 end
