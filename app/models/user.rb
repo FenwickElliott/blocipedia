c9fb0ce1a7af7d932c824501804dd5a8dc334c06
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-03-16 10:45:52 -0400

    sign up page fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :wikis
+
+  after_initialize :init
+
+  enum role: [:standard, :premium, :admin]
+
+  def init
+    self.role ||= 0
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,26 @@
 require 'random_data'
 
-# user = User.new
-# user.update_attributes(
-#     email: 'charles@FenwickElliott.io',
-#     password: '123456'
-#     )
+user = User.new
+user.update_attributes(
+    email: 'admin@eg.com',
+    password: '123456',
+    role: :admin
+    )
+
+user = User.new
+user.update_attributes(
+    email: 'premium@eg.com',
+    password: '123456',
+    role: :premium
+    )
+
+user = User.new
+user.update_attributes(
+    email: 'standard@eg.com',
+    password: '123456',
+    role: :standard
+    )
+
 
 for i in 1..25 do
   Wiki.create!(
@@ -13,4 +29,6 @@ for i in 1..25 do
     user_id: 1
   )
 end
+
+puts "#{User.count} users"
 puts "#{Wiki.count} Wikis"
